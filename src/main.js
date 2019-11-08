c0d04bc0d7c7fca02becf8155245b4b46b6d73fa
syvue <1906094@qq.com>
2019-11-08 14:17:04 +0800

    轮播图，分支

--- src/main.js
+++ src/main.js
@@ -3,15 +3,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 导入fastclick解决手机或浏览器可能的300毫秒延迟的问题
 import './assets/styles/reset.css'
 // 重置所有样式
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 // 解决移动端1像素边框问题
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 // 调用fastClick绑定到document.body上
 
 /* eslint-disable no-new */
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,33 @@
+<template>
+   <swiper :options="swiperOption" >
+    <!-- slides -->
+    <swiper-slide><img class="img-d" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/f9511d2b13e02be5aa3969a68d63a633.jpg_750x200_1c250e36.jpg" ></swiper-slide>
+    <swiper-slide><img class="img-d" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/0a05e383c888d57d83751171b9c68fd3.jpg_750x200_c82bac08.jpg"></swiper-slide>
+    <swiper-slide>I'm Slide 3</swiper-slide>
+    <swiper-slide>I'm Slide 4</swiper-slide>
+    <swiper-slide>I'm Slide 5</swiper-slide>
+    <swiper-slide>I'm Slide 6</swiper-slide>
+    <swiper-slide>I'm Slide 7</swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+    <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div>
+    <div class="swiper-scrollbar"   slot="scrollbar"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+      return {
+          swiperOtion: {}
+      }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .img-d
+      width 100%
+</style> 
