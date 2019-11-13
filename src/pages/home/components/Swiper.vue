e456088dd1c4fa86518526082dd787996b63a1ff
syvue <1906094@qq.com>
2019-11-13 10:04:30 +0800

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,19 +1,15 @@
 <template>
-   <swiper :options="swiperOption" >
-    <!-- slides -->
-    <swiper-slide><img class="img-d" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/f9511d2b13e02be5aa3969a68d63a633.jpg_750x200_1c250e36.jpg" ></swiper-slide>
-    <swiper-slide><img class="img-d" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/0a05e383c888d57d83751171b9c68fd3.jpg_750x200_c82bac08.jpg"></swiper-slide>
-    <swiper-slide>I'm Slide 3</swiper-slide>
-    <swiper-slide>I'm Slide 4</swiper-slide>
-    <swiper-slide>I'm Slide 5</swiper-slide>
-    <swiper-slide>I'm Slide 6</swiper-slide>
-    <swiper-slide>I'm Slide 7</swiper-slide>
-    <!-- Optional controls -->
-    <div class="swiper-pagination"  slot="pagination"></div>
-    <div class="swiper-button-prev" slot="button-prev"></div>
-    <div class="swiper-button-next" slot="button-next"></div>
-    <div class="swiper-scrollbar"   slot="scrollbar"></div>
-  </swiper>
+  <div class="wrapper">
+      <swiper :options="swiperOption" >
+        <!-- slides -->
+        <swiper-slide v-for='item of swiperList' :key='item.id'>
+          <img class="swiper-img" :src="item.imgUrl" >
+        </swiper-slide>
+       
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+  </div>
 </template>
 
 <script>
@@ -21,13 +17,35 @@ export default {
   name: 'HomeSwiper',
   data() {
       return {
-          swiperOtion: {}
+          swiperOption: {
+            pagination: '.swiper-pagination',
+            loop:true
+          },
+          swiperList:[{
+            id: '001',
+            imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/f9511d2b13e02be5aa3969a68d63a633.jpg_750x200_1c250e36.jpg'
+          },{
+            id: '002',
+            imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/0a05e383c888d57d83751171b9c68fd3.jpg_750x200_c82bac08.jpg'
+          },{
+            id: '003',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1706/b8/a8e8ff02b094c802.jpg_750x200_ddaec8e5.jpg'
+          }]
       }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-    .img-d
-      width 100%
+    .wrapper >>> .swiper-pagination-bullet-active
+      background: #fff
+    .wrapper
+      overflow hidden
+      width: 100%
+      height: 0
+      padding-bottom: 26.66%
+      background: #ccc
+      .swiper-img
+        width 100%
+      
 </style> 
