821b2fabed0e46e146f3f48314a7bf5fa8bca4c6
syvue <1906094@qq.com>
2019-11-18 04:12:49 +0800

    swiper done

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,17 +1,14 @@
 <template>
-     <swiper :options="swiperOption" >
-    <!-- slides -->
-    <swiper-slide>
-    <img class='swiper-img' src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/c8762210baff5ade1a3b2fe0ab914992.png_750x200_9bbb2f51.png' />
-    </swiper-slide>
-    <swiper-slide>
-    <img class='swiper-img' src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/1d8faa9f4d49b77ca1e61e2b478260f7.jpg_750x200_25cf76e2.jpg'/>
-    </swiper-slide>
-    <swiper-slide>I'm Slide 3</swiper-slide>
-    <swiper-slide>I'm Slide 4</swiper-slide>
-    <!-- Optional controls -->
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
+    <div class="wrapper">
+      <swiper :options="swiperOption" >
+        <!-- slides -->
+        <swiper-slide v-for='item of swiperList' :key='item.id'>
+        <img class='swiper-img' :src='item.imgUrl' />
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
 </template>
 
 <script>
@@ -19,14 +16,38 @@ export default {
   name: 'HomeSwiper',
   data() {
     return {
-      swiperOption:{}
+      swiperOption:{
+        pagination: '.swiper-pagination',
+        loop:true 
+      },
+      swiperList:[{
+        id:'001',
+        imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/c8762210baff5ade1a3b2fe0ab914992.png_750x200_9bbb2f51.png'
+      },{
+        id:'002',
+        imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/1d8faa9f4d49b77ca1e61e2b478260f7.jpg_750x200_25cf76e2.jpg'
+      },{
+        id:'003',
+        imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/e7eac5a5e99e6517c61b953c0fece63b.jpg_750x200_4b701cf8.jpg'
+      },{
+        id:'004',
+        imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/6da1597f11456f5a42a1818ab3f6fa08.jpg_750x200_31b64c4a.jpg'
+      }]
     }
   }
  
 }
-</script >
+</script>
+
 
 <style lang='stylus' scoped>
- .swiper-img
-   width: 100%
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.66%
+    .swiper-img
+      width: 100%
 </style>
--- web/init.md
+++ web/init.md
@@ -632,6 +632,7 @@ export default {
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+
 export default {
   name: 'Home',
   components: {
@@ -648,3 +649,67 @@ export default {
 ```
 * Home.vue
 
+#### 优化轮播图
+
+> 在完成轮播图的基本功能之后，我们要需要继续优化代码来完善功能。我们先看代码
+
+``` javascript
+<template>
+    <div class="wrapper">
+      <!-- 在swiper外层添加一个类名为wrapper的div标签 -->
+      <swiper :options="swiperOption" >
+        <!-- slides -->
+        <swiper-slide v-for='item of swiperList' :key='item.id'>
+        <img class='swiper-img' :src='item.imgUrl' />
+        </swiper-slide>
+        <!-- Optional controls -->
+        <!-- 利用v-for 来循环遍历 swiperList数组，添加绑定key值来实现轮播的顺序-->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption:{
+        pagination: '.swiper-pagination',
+        //添加pagination选项并赋值.swiper-pagination来实现轮播图上圆点选项
+        loop:true 
+        //添加loop选项来实现轮播图的循环 
+      },
+      swiperList:[{
+        id:'001',
+        imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/c8762210baff5ade1a3b2fe0ab914992.png_750x200_9bbb2f51.png'
+      },{
+        id:'002',
+        imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/1d8faa9f4d49b77ca1e61e2b478260f7.jpg_750x200_25cf76e2.jpg'
+      },{
+        id:'003',
+        imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/e7eac5a5e99e6517c61b953c0fece63b.jpg_750x200_4b701cf8.jpg'
+      },{
+        id:'004',
+        imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/6da1597f11456f5a42a1818ab3f6fa08.jpg_750x200_31b64c4a.jpg'
+      }]
+      //添加swiperList数组来实现管理轮播图播放的顺序和图片地址
+    }
+  }
+ 
+}
+</script>
+
+<style lang='stylus' scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.66%
+    .swiper-img
+      width: 100%
+</style>
+
+```
